794ab9d00fc00a4193882df9e58f5a1859b70d11
monzir.o <monzer.o@sybertechnology.com>
2020-11-22 14:25:53 +0200

    Initial project setup

---
+++ SwiftMath/SwiftMath.h
@@ -0,0 +1,18 @@
+//
+//  SwiftMath.h
+//  SwiftMath
+//
+//  Created by Syber Expertise on 22/11/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftMath.
+FOUNDATION_EXPORT double SwiftMathVersionNumber;
+
+//! Project version string for SwiftMath.
+FOUNDATION_EXPORT const unsigned char SwiftMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMath/PublicHeader.h>
+
+
